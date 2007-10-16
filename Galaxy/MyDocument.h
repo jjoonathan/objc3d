52290329ba6ce662ad4cad46382ee4862641dad0
Jonathan deWerd <jjoonathan@gmail.com>
2007-10-16 06:17:20 -0600

    Initial import of Galaxy to project root dir

---
+++ Galaxy/MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  Galaxy
+//
+//  Created by Jonathan deWerd on 9/17/07.
+//  Copyright __MyCompanyName__ 2007 . All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+}
+@end
---
+++ Galaxy/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Galaxy
+//
+//  Created by Jonathan deWerd on 9/17/07.
+//  Copyright __MyCompanyName__ 2007 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **) argv);
+}
