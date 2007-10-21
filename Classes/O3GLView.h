41f63abc9bf11c7c7fd33071ceddd88864f6bdd5
Jonathan deWerd <jjoonathan@gmail.com>
2007-10-21 14:35:19 -0600

    Added new view class, updated build settings

---
+++ Classes/O3GLView.h
@@ -0,0 +1,13 @@
+//
+//  O3GLView.h
+//  ObjC3D
+//
+//  Created by Jonathan deWerd on 10/18/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+@interface O3GLView : NSOpenGLView {
+
+}
+
+@end
---
+++ Classes/O3GLView.mm
@@ -0,0 +1,14 @@
+//
+//  O3GLView.mm
+//  ObjC3D
+//
+//  Created by Jonathan deWerd on 10/18/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import "O3GLView.h"
+
+
+@implementation O3GLView
+
+@end
